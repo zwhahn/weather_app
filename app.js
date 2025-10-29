a9e9bfdba75f4eba9e614428c15d296b910377b4
zwhahn <117476820+zwhahn@users.noreply.github.com>
2025-10-29 14:42:49 -0400

    Get current temp

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ async function getWeather () {
     const response = await fetch('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/19119/?key=R8KYRLHHRZ6LPWGXCEJY4MLDX');
     const weatherData = await response.json();
     console.log(weatherData);
+    const currentTemp = weatherData.currentConditions.temp;
+    console.log(currentTemp);
 }
 
 getWeather();
