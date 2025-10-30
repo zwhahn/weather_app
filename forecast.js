2946860fa37bc615e9e8d602ac901a1686adca8d
zwhahn <117476820+zwhahn@users.noreply.github.com>
2025-10-30 10:38:47 -0400

    Add labels for max/min temps, seperate weekday and date

--- forecast.js
+++ forecast.js
@@ -16,7 +16,6 @@ switchUnitsBtn.addEventListener('click', function(event) {
         updateDayDivs(weatherForecast_F);
         switchUnitsBtn.textContent = 'C';
     }
-    console.log(`current units: ${units}`);
 })
 
 if (place) {
@@ -27,13 +26,13 @@ async function getWeather () {
     const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${place}/?key=R8KYRLHHRZ6LPWGXCEJY4MLDX`);
     const weatherData = await response.json();
     console.log(weatherData);
-    const currentTemp = weatherData.currentConditions.temp;
     weatherForecast_F = []
     weatherForecast_C = []
     for (let i =0; i < 5; i++){
         day = weatherData.days[i]
         weatherForecast_F.push({
-            date: day.datetime,
+            weekday: getWeekday(day.datetime),
+            date: formatDate(day.datetime),
             tempMax: Math.round(day.tempmax),
             tempMin: Math.round(day.tempmin),
             conditions: day.conditions,
@@ -53,6 +52,7 @@ async function getWeather () {
     else {
         populateDayDivs(weatherForecast_C)
     }
+    console.log(weatherForecast_F);
 }
 
 function populateDayDivs(weatherForecast){
@@ -62,21 +62,41 @@ function populateDayDivs(weatherForecast){
         const dayDiv = document.createElement('div');
         dayDiv.classList.add('day');
 
+
+        const weekdayDiv = document.createElement('h2');
+        weekdayDiv.innerHTML = `${dayData.weekday}`;
+
+
         const dateDiv = document.createElement('div');
         dateDiv.textContent = dayData.date;
 
+
+        const tempMaxHolder = document.createElement('div');
+        tempMaxHolder.classList.add('data-holder');
+        const tempMaxTitle = document.createElement('div');
+        tempMaxTitle.innerHTML = 'High:'
+        tempMaxTitle.classList.add('data-title')
         const tempMaxDiv = document.createElement('div');
         tempMaxDiv.textContent = dayData.tempMax;
         tempMaxDiv.classList.add('temp-max');
+        tempMaxHolder.append(tempMaxTitle, tempMaxDiv);
 
+
+        const tempMinHolder = document.createElement('div');
+        tempMinHolder.classList.add('data-holder');
+        const tempMinTitle = document.createElement('div');
+        tempMinTitle.innerHTML = 'Low:'
+        tempMinTitle.classList.add('data-title')
         const tempMinDiv = document.createElement('div');
         tempMinDiv.textContent = dayData.tempMin;
         tempMinDiv.classList.add('temp-min');
+        tempMinHolder.append(tempMinTitle, tempMinDiv);
+
 
         const conditionsDiv = document.createElement('div');
         conditionsDiv.textContent = dayData.conditions;
 
-        dayDiv.append(dateDiv, tempMaxDiv, tempMinDiv, conditionsDiv);
+        dayDiv.append(weekdayDiv, dateDiv, conditionsDiv, tempMaxHolder, tempMinHolder);
 
         flexContainer.append(dayDiv);
     });
@@ -96,4 +116,30 @@ function updateDayDivs(weatherForecast){
 
 function fahrenheitToCelsius(fahrenheit){
     return (fahrenheit - 32) * 5 / 9;
+}
+
+function getWeekday(dateString) {
+    const date = new Date(dateString);
+    const dateWeekday = date.toLocaleDateString("en-US", {
+            weekday: "long",
+    });
+
+    const todayWeekday = new Date().toLocaleDateString("en-US", {
+            weekday: "long",
+    });
+
+    if (dateWeekday == todayWeekday) {
+        return 'Today'
+    }
+    else {
+        return dateWeekday;
+    }
+}
+
+function formatDate(dateString) {
+    const date = new Date(dateString);
+    return date.toLocaleDateString("en-US", {
+        month: "short",
+        day: "numeric"     
+    });
 }
